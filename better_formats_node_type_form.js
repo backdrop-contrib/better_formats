85961a0f4c02e066ab8bb4338f0b3cbf7828e045
dragonwize <dragonwize@137882.no-reply.drupal.org>
2008-12-23 20:57:39 +0000

    fixed bug with allowed formats per node type javascript helper

--- better_formats_node_type_form.js
+++ better_formats_node_type_form.js
@@ -9,24 +9,44 @@
  * Also hides formats that are not available per the Allowed checkboxes.
  */
  
- 
 function better_formats_toggle_formats(el) {
   $('select.bf-default-formats option').each(function() {
     var option = $(this);
     if (option.val() == el.val()) {
       if (el.attr('checked')) {
+        // hide all formats except site default when the first box is checked
+        if (Drupal.settings.better_formats.num_checked === 0) {
+          $('select.bf-default-formats option[value != "0"]').hide();
+        }
+        Drupal.settings.better_formats.num_checked += 1;
         option.show();
       } else {
         option.removeAttr('selected').hide();
+        // keep num_checked from going below zero
+        if (Drupal.settings.better_formats.num_checked > 0) {
+          Drupal.settings.better_formats.num_checked -= 1;
+        }
+        // show all globally allowed formats if no boxes are checked
+        if (Drupal.settings.better_formats.num_checked === 0) {
+          // show global formats available to roles 
+          // because no format allowed boxes are checked
+          $('select.bf-default-formats option').show();
+        }
       }
     }
   });
 }
 
+function better_formats_show_global_formats() {
+  $('select.bf-default-formats option').show();
+}
+
 $(document).ready(function() {
+  // set default settings check for use of global allowed formats
+  Drupal.settings.better_formats = {"num_checked" : 0};
   // collapsing the input format setting after the weight columns have been hidden
   $('.input-format-settings > legend > a').click();
-  // 
+  // add hide/show events for allowed formats
   $('input.bf-allowed-formats').click(function() {
     better_formats_toggle_formats($(this));
   }).each(function() {
