b51ceef414f7b3632807d4c9f9c8a6cb11b2321f
dragonwize <dragonwize@137882.no-reply.drupal.org>
2008-12-24 08:00:54 +0000

    More fixes and enhancements to the JS helper for the content type form

--- better_formats_node_type_form.js
+++ better_formats_node_type_form.js
@@ -9,47 +9,59 @@
  * Also hides formats that are not available per the Allowed checkboxes.
  */
  
-function better_formats_toggle_formats(el) {
-  $('select.bf-default-formats option').each(function() {
-    var option = $(this);
-    if (option.val() == el.val()) {
-      if (el.attr('checked')) {
-        // hide all formats except site default when the first box is checked
-        if (Drupal.settings.better_formats.num_checked === 0) {
-          $('select.bf-default-formats option[value != "0"]').hide();
-        }
-        Drupal.settings.better_formats.num_checked += 1;
-        option.show();
-      } else {
-        option.removeAttr('selected').hide();
-        // keep num_checked from going below zero
-        if (Drupal.settings.better_formats.num_checked > 0) {
-          Drupal.settings.better_formats.num_checked -= 1;
-        }
-        // show all globally allowed formats if no boxes are checked
-        if (Drupal.settings.better_formats.num_checked === 0) {
-          // show global formats available to roles 
-          // because no format allowed boxes are checked
-          $('select.bf-default-formats option').show();
-        }
-      }
-    }
-  });
-}
-
-function better_formats_show_global_formats() {
-  $('select.bf-default-formats option').show();
-}
-
-$(document).ready(function() {
+function better_formats_init() {
   // set default settings check for use of global allowed formats
-  Drupal.settings.better_formats = {"num_checked" : 0};
+  Drupal.settings.better_formats = {"num_checked" : $('input.bf-allowed-formats:checked').length};
+  
   // collapsing the input format setting after the weight columns have been hidden
   $('.input-format-settings > legend > a').click();
+  
   // add hide/show events for allowed formats
-  $('input.bf-allowed-formats').click(function() {
-    better_formats_toggle_formats($(this));
-  }).each(function() {
+  var format_boxes = $('input.bf-allowed-formats');
+  format_boxes.click(function() {
     better_formats_toggle_formats($(this));
   });
-});
+  if (Drupal.settings.better_formats.num_checked > 0) {
+    format_boxes.each(function() {
+      better_formats_toggle_formats($(this), true);
+    });
+  }
+}
+
+function better_formats_toggle_formats(el, init) {
+  // hide all formats except site default when the first box is checked
+  if (Drupal.settings.better_formats.num_checked === 0) {
+    $('select.bf-default-formats option[value != "0"][value != "' + el.val() + '"]').removeAttr('selected').hide();
+  }
+  
+  $('select.bf-default-formats option[value = "' + el.val() + '"]').each(function() {
+    var option = $(this);
+      if (el.attr('checked')) {
+        option.show();
+      } 
+      else {
+        option.removeAttr('selected').hide();
+      }
+  });
+  
+  // do not modify count on intial run
+  if ( ! init) {
+    if (el.attr('checked')) {
+      Drupal.settings.better_formats.num_checked += 1;
+    }
+    else if (Drupal.settings.better_formats.num_checked > 0) {
+      // keep num_checked from going below zero
+      Drupal.settings.better_formats.num_checked -= 1;
+    }
+  }
+  
+  // show all globally allowed formats if no boxes are checked
+  if (Drupal.settings.better_formats.num_checked === 0) {
+    // show global formats available to roles 
+    // because no format allowed boxes are checked
+    $('select.bf-default-formats option').show();
+  }
+}
+
+
+$(document).ready(better_formats_init);
